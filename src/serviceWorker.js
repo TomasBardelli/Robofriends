812983759f5f6b8716555f4a144523b5496ae5d3
TomasBardelli <68757908+TomasBardelli@users.noreply.github.com>
2021-01-18 12:57:01 -0300

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,11 +1,9 @@
 const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.0/8 are considered localhost for IPv4.
-    window.location.hostname.match(
+  window.location.hostname === 'localhost' ||    
+  window.location.hostname === '[::1]' ||   
+  window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+  )
 );
 
 export function register(config) {
@@ -15,7 +13,7 @@ export function register(config) {
       return;
     }
 
-    window.addEventListener('load', () => {
+window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
